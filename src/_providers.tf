92e901bfb2b644981ce9431c08dcf09906e1f5c2
chrisghill <chris@massdriver.cloud>
2023-04-27 11:45:48 -0600

    Initial working

---
+++ src/_providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    massdriver = {
+      source  = "massdriver-cloud/massdriver"
+      version = "~> 1.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.gcp_authentication.data.project_id
+  credentials = jsonencode(var.gcp_authentication.data)
+  region      = var.dataset.specs.gcp.region
+}
